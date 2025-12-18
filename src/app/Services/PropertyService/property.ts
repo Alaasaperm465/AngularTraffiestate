f062a4fb71fcf8b017f3cc2497b1ac19c636c1ae
abrar <abrabad02mail.com>
2025-12-18 20:54:32 +0200

    finsh the interceptor

---
+++ src/app/Services/PropertyService/property.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IProperty } from '../../models/iproperty';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PropertyService {
+  private apiUrl = 'https://localhost:7190/api/properties'; // غيرها حسب API الخاص بك
+
+  constructor(private http: HttpClient) {}
+
+  // جلب كل العقارات
+  getAllProperties(): Observable<IProperty[]> {
+    return this.http.get<IProperty[]>(this.apiUrl);
+  }
+
+  // جلب عقار واحد حسب الـ id
+  getPropertyById(id: number): Observable<IProperty> {
+    return this.http.get<IProperty>(`${this.apiUrl}/${id}`);
+  }
+
+  // إنشاء عقار جديد
+  createProperty(property: IProperty): Observable<IProperty> {
+    return this.http.post<IProperty>(this.apiUrl, property);
+  }
+
+  // تعديل عقار
+  updateProperty(id: number, property: IProperty): Observable<IProperty> {
+    return this.http.put<IProperty>(`${this.apiUrl}/${id}`, property);
+  }
+
+  // حذف عقار
+  deleteProperty(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
