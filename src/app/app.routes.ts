7d5a94e27b49bbbe52586e6b63718f2d012c057b
bassem <basem.abulgheit@gmail.com>
2025-12-24 15:34:23 +0200

    Add About us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,6 +20,7 @@ import { Land } from './land/land';
 import { ProfileComponent } from './Component/profile/profile';
 import { EditProfile } from './Component/edit-profile/edit-profile';
 import { ContactUs } from './Component/contact-us/contact-us';
+import { AboutUs } from './Component/about-us/about-us';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -41,6 +42,7 @@ export const routes: Routes = [
   { path: 'reset-password', component: ResetPassword },
   { path: 'success', component: SuccessComponent },
   { path: 'contact', component: ContactUs },
+  { path: 'about', component: AboutUs },
   { path: '**', component: NotFound },
 
   //************************************************************************************************************************** */
