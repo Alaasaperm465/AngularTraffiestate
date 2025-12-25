a6e38d5ce73f418d5e03830ee7827851f31e11d6
abrar <abrabad02mail.com>
2025-12-25 22:40:36 +0200

    shahiii

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,6 +22,7 @@ import { EditProfile } from './Component/edit-profile/edit-profile';
 import { ContactUs } from './Component/contact-us/contact-us';
 import { AboutUs } from './Component/about-us/about-us';
 import { EditPropertyComponent } from './Component/edit-property/edit-property';
+import { ChatbotComponent } from './chatbot/chatbot';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -46,6 +47,7 @@ export const routes: Routes = [
   { path: 'contact', component: ContactUs },
   { path: 'about', component: AboutUs },
   { path: 'editproperty', component: EditPropertyComponent },
+  { path: 'inquiries', component: ChatbotComponent, title: 'Inquiries with Farida' },
   { path: '**', component: NotFound },
 
   //************************************************************************************************************************** */
